82bc871ab6e1ba750184b9a45906ed38111f7432
Zebula Sampedro <sampedro@colorado.edu>
2016-08-03 17:45:15 -0600

    Partially sorted out outline issue with md blocks.

--- js/docs.js
+++ js/docs.js
@@ -2,10 +2,14 @@
 
 (function renderMarkdown() {
   marked.setOptions({
-    gfm: true
+    gfm: true,
+    highlight: function (code) {
+      console.log(code);
+      return hljs.highlightAuto(code).value;
+    }
   });
   var mdDivs = document.getElementsByClassName('doc-markdown');
   for (var i=0;i<mdDivs.length;i++) {
-    mdDivs[i].innerHTML = marked(mdDivs[i].innerText);
+    mdDivs[i].innerHTML = marked(mdDivs[i].innerHTML);
   }
 }());
