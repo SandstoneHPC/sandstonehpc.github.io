d34ebf278f137f66e0e916e5aa1419ba8e0891f5
Zebula Sampedro <sampedro@colorado.edu>
2016-10-19 15:08:43 -0600

    Fixed anchor scroll issue.

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -3,8 +3,8 @@
 (function buildAnchorLinks() {
   var headings = $('h2,h3');
   headings.each(function() {
-    var link = encodeURIComponent(this.innerText.toLowerCase());
-    var linkHTML = '<a name="' + link + '" href="#' + link + '">' + this.innerText + '</a>';
+    var link = this.id;
+    var linkHTML = '<a href="#' + link + '">' + this.innerText + '</a>';
     this.innerHTML = linkHTML;
   });
 }());
